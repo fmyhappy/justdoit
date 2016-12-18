8602eb7a41e4e52845e945da55b6f97c8a1055ff
fmy <1126968939@qq.com>
2016-12-18 15:49:24 +0800

    second commit

---
+++ src/test/java/com/test/TestCommit.java
@@ -0,0 +1,15 @@
+package com.test;
+
+import com.cat.justdoit.domain.user.User;
+
+import java.util.ArrayList;
+
+/**
+ * Created by FMY on 2016/12/18 0018.
+ */
+public class TestCommit {
+    public void  test1(){
+        new ArrayList<User>();
+    }
+    //
+}
